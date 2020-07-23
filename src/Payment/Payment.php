455430d41600a4bae2eb97e27beec138d8c7d718
Silvan van der Veen <silvan@targetmedia.eu>
2020-07-23 15:54:11 +0200

    Add Giropay and EPS

--- src/Payment/Payment.php
+++ src/Payment/Payment.php
@@ -14,6 +14,8 @@ class Payment
     public const PAY_SAFE_CARD = 'PSC';
     public const PAY_PAL = 'PYP';
     public const SOFORT = 'SOF';
+    public const GIROPAY = 'GIP';
+    public const EPS = 'EPS';
 
     public const METHODS = [
         self::AFTERPAY,
@@ -23,5 +25,7 @@ class Payment
         self::PAY_SAFE_CARD,
         self::PAY_PAL,
         self::SOFORT,
+        self::GIROPAY,
+        self::EPS
     ];
 }
